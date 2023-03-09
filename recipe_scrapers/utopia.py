f2ad1698cbb93815d3cc556755e36396ec5151a6
vdorastie <negruvlad_007@yahoo.com>
2023-03-09 11:21:24 +0200

    Added utopia fix2

--- recipe_scrapers/utopia.py
+++ recipe_scrapers/utopia.py
@@ -12,8 +12,8 @@ class Utopia(AbstractScraper):
 
     def instructions(self):
         instructions_div = self.soup.find("div", {"id": "main-content"}).find_all("ol")
-		instructions_ils = [instruction_ol.find_all("li") for instruction_ol in instructions_div]
-		instructions_ils = [item for sublist in instructions_ils for item in sublist]
+        instructions_ils = [instruction_ol.find_all("li") for instruction_ol in instructions_div]
+        instructions_ils = [item for sublist in instructions_ils for item in sublist]
 		
         instructions = [
             normalize_string(instruction.get_text(strip=True, separator=" "))
